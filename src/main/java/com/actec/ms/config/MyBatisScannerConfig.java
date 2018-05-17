051acae4fb1391e195450a542ea5e4c6cb1c7be4
SuperYangAI <741461014@qq.com>
2018-05-17 21:43:10 +0800

    添加整合通用Mapper注释

--- src/main/java/com/actec/ms/config/MyBatisScannerConfig.java
+++ src/main/java/com/actec/ms/config/MyBatisScannerConfig.java
@@ -1,5 +1,10 @@
 package com.actec.ms.config;
-
+/**
+ * 通用Mapper导入
+ * 1、maven引入tk mapper包
+ * 2、org.mybatis.spring.mapper.MapperScannerConfigurer改成
+ * tk.mybatis.spring.mapper.MapperScannerConfigurer
+ * ***/
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
